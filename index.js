24f4c4e4720a58b5e2ddd374545239fce81a2549
malavikasmenon <malavikamenon2000@gmail.com>
2020-09-07 17:34:58 +0530

    Added index.js.

---
+++ index.js
@@ -0,0 +1,47 @@
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot("1350049849:AAFly91P-hNi66Llph1Q0MO3JH-YHb19MGA", { polling: true });
+
+
+bot.onText(/\/anagram/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const word = match.input.split(' ')[1];
+
+    if (word === undefined) {
+        bot.sendMessage(
+            chatId,
+            'Please provide a word!',
+        );
+        return;
+    }
+
+
+
+    const stringPermutations = str => {
+        if (str.length <= 2) return str.length === 2 ? [str, str[1] + str[0]] : [str];
+        return str
+            .split('')
+            .reduce(
+                (acc, letter, i) =>
+                acc.concat(stringPermutations(str.slice(0, i) + str.slice(i + 1)).map(val => letter + val)), []
+            );
+    };
+
+    let message = stringPermutations(word);
+
+    if (message.length > 10) {
+        let deletion = message.length - 10;
+        message.splice(10, deletion);
+    }
+
+    let finalMessage = "";
+    for (i = 0; i < message.length; i++) {
+        finalMessage = finalMessage + message[i] + '\n'
+    }
+
+    console.log(finalMessage)
+
+    bot.sendMessage(
+        chatId,
+        ` ${finalMessage}`,
+    );
+});
