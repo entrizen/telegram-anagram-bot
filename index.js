cd1155bba7f218a6d6ba9b1aff92f0b9ad4a91d8
malavikasmenon <malavikamenon2000@gmail.com>
2020-09-07 20:42:29 +0530

    Webhook correction

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+const TOKEN = process.env.TELEGRAM_TOKEN || '1350049849:AAFly91P-hNi66Llph1Q0MO3JH-YHb19MGA';
 const TelegramBot = require('node-telegram-bot-api');
+const options = {
+    webHook: {
+        // Just use 443 directly
+        port: 443
+    }
+};
+// You can use 'now alias <your deployment url> <custom url>' to assign fixed
+// domain.
+// See: https://zeit.co/blog/now-alias
+// Or just use NOW_URL to get deployment url from env.
+const url = 'https://telegram-bot-omega.vercel.app/' || process.env.NOW_URL;
+const bot = new TelegramBot(TOKEN, options);
+
+
+// This informs the Telegram servers of the new webhook.
+// Note: we do not need to pass in the cert, as it already provided
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+    bot.sendMessage(msg.chat.id, 'I am alive on Zeit Now!');
+});
+
+
+/*const TelegramBot = require('node-telegram-bot-api');
+const
 const bot = new TelegramBot("1350049849:AAFly91P-hNi66Llph1Q0MO3JH-YHb19MGA", { polling: true });
 
 
@@ -44,4 +72,4 @@ bot.onText(/\/anagram/, (msg, match) => {
         chatId,
         ` ${finalMessage}`,
     );
-});
+}); */
